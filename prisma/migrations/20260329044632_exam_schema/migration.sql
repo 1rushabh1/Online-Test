c6aa2993da9bc1e10ced3b752cfff0349418005d
1rushabh1 <rushabhthaware1>
2026-03-29 10:18:36 +0530

    updated project before deployment

---
+++ prisma/migrations/20260329044632_exam_schema/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "Exam" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Exam_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Question" (
+    "id" SERIAL NOT NULL,
+    "text" TEXT NOT NULL,
+    "options" TEXT NOT NULL,
+    "answer" TEXT NOT NULL,
+    "examId" INTEGER NOT NULL,
+
+    CONSTRAINT "Question_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Exam" ADD CONSTRAINT "Exam_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Question" ADD CONSTRAINT "Question_examId_fkey" FOREIGN KEY ("examId") REFERENCES "Exam"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
